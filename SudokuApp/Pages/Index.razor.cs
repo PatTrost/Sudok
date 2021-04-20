16b967212b08b28228f57915a749a1f052999e01
Raxxum <Zyphon.88@gmail.com>
2021-04-20 13:47:39 +1000

    Switch to single page site

---
+++ SudokuApp/Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SudokuApp.Shared;
+
+namespace SudokuApp.Pages
+{
+    partial class Index
+    {
+        public string Givens { get; set; }
+
+        private string TempText = "";
+
+        bool _drawerOpen = true;
+
+        void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+
+        private void MakeSudoku()
+        {
+            TempText = "Sudoku!";
+        }
+    }
+}
